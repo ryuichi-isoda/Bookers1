a5c72fe3a8be07cf80a1a4336ba90b0701eb4f89
isoda <ryuichi.isoda1007@gmail.com>
2019-09-23 01:28:15 +0900

    [Update] books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,10 +14,12 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      # redirect_to book_path(@book.id)と同じ意味
+      redirect_to @book
     else
       @books = Book.all
-      render action: :index
+      # render action: :indexと同じ意味
+      render :index
     end
   end
 
@@ -29,7 +31,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+      # redirect_to book_path(@book.id)と同じ意味
+      redirect_to @book
     else
       render template: "books/edit"
     end
@@ -39,9 +42,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.destroy
       flash[:notice] = "Book was successfully destroyed."
-     redirect_to books_path
+      redirect_to books_path
     else
-     render action: :index
+      # render action: :indexと同じ意味
+      render :index
     end
   end
 
